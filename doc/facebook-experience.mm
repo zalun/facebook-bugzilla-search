76b9508b7d7e9ff0883ce0e8a5950ba011ae7612
Piotr Zalewa <piotr@zalewa.info>
2012-04-13 12:52:08 +0200

    mindmap added

---
+++ doc/facebook-experience.mm
@@ -0,0 +1,108 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1334229384108" ID="ID_1337386578" MODIFIED="1334231383746" TEXT="Apptastic">
+<node CREATED="1334230335694" ID="ID_1647769693" MODIFIED="1334230339989" POSITION="right" TEXT="Hello World">
+<node CREATED="1334230350925" ID="ID_1261043534" MODIFIED="1334231379488" TEXT="I am trying to come to creating my first app as soon as possible"/>
+<node CREATED="1334229734389" ID="ID_1888476299" MODIFIED="1334230343731" TEXT="https://developers.facebook.com">
+<node CREATED="1334229745452" ID="ID_1949757174" MODIFIED="1334231195451">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      This asks you what type of the app one would like to create:&#160;
+    </p>
+    <ul>
+      <li>
+        for websites
+      </li>
+      <li>
+        for mobiles
+      </li>
+      <li>
+        on facebook
+      </li>
+    </ul>
+    <p>
+      I've chosen on FB as it looks more like &quot;native&quot; app
+    </p>
+  </body>
+</html></richcontent>
+<arrowlink DESTINATION="ID_1650545613" ENDARROW="Default" ENDINCLINATION="304;0;" ID="Arrow_ID_720699430" STARTARROW="None" STARTINCLINATION="304;0;"/>
+</node>
+</node>
+<node CREATED="1334229654224" ID="ID_1650545613" MODIFIED="1334231195450" TEXT="https://developers.facebook.com/docs/guides/canvas/">
+<node CREATED="1334229947165" ID="ID_1141479097" MODIFIED="1334229953006" TEXT="3 concepts"/>
+<node CREATED="1334229953536" ID="ID_1952132652" MODIFIED="1334229956829" TEXT="Canvas Page">
+<node CREATED="1334229958850" ID="ID_146445334" MODIFIED="1334229983800" TEXT="Apps on Facebook are web apps that are loaded in the context of Facebook in what we refer to as a Canvas Page"/>
+<node CREATED="1334230047885" ID="ID_110290443" MODIFIED="1334230052632" TEXT="The canvaThe canvas chrome is 760px width by default but you can set it to be a Fluid Canvass chrome is 760px width by default but you can set it to be a Fluid Canvas"/>
+<node CREATED="1334230078647" ID="ID_387972974" MODIFIED="1334230212451" TEXT="If you want to get started with building an App on Facebook right away, please read the getting started tutorial."/>
+</node>
+</node>
+<node CREATED="1334230202313" ID="ID_1972710509" MODIFIED="1334230343711" TEXT="https://developers.facebook.com/docs/appsonfacebook/tutorial/">
+<node CREATED="1334230252650" ID="ID_481611358" MODIFIED="1334231187206" TEXT="Start by visiting the Developer App">
+<arrowlink DESTINATION="ID_599484138" ENDARROW="Default" ENDINCLINATION="434;0;" ID="Arrow_ID_1246452279" STARTARROW="None" STARTINCLINATION="434;0;"/>
+</node>
+</node>
+<node CREATED="1334230309030" ID="ID_599484138" MODIFIED="1334231187205" TEXT="https://developers.facebook.com/apps">
+<node CREATED="1334230510361" ID="ID_1498371717" MODIFIED="1334230514422" TEXT="Create New app">
+<node CREATED="1334230515685" ID="ID_1745612238" MODIFIED="1334230523501" TEXT="Reading about hosting on Heroku">
+<node CREATED="1334230525838" ID="ID_1125595475" MODIFIED="1334230529550" TEXT="https://developers.facebook.com/blog/post/558/"/>
+</node>
+<node CREATED="1334230539803" ID="ID_503626087" MODIFIED="1334232577053" TEXT="Decided to go with it as this would be a default choice for many beginners"/>
+<node CREATED="1334230617670" ID="ID_1265583378" MODIFIED="1334230634093" TEXT="The app namespace has a bit unclear explanation for a beginner like me">
+<node CREATED="1334230636609" ID="ID_616373829" MODIFIED="1334230748503" TEXT="The app namespace is used for defining custom Open Graph actions and objects (e.g. namespace:action) and for the URL for Canvas apps used on Facebook (e.g. http://apps.facebook.com/namespace)"/>
+<node CREATED="1334230752013" ID="ID_1574981286" MODIFIED="1334230768947" TEXT="There was no mention about OpenGraph in my path to Create an app"/>
+</node>
+<node CREATED="1334230771002" ID="ID_1057781103" MODIFIED="1334230847113" TEXT="I&apos;ve chosen to create a namespace &quot;bugzilla-search&quot; as bugzilla was unavailable"/>
+<node CREATED="1334230886505" ID="ID_1906409597" MODIFIED="1334230944222" TEXT="After so much burden I received an information that my account is not verified (?!) Going through the verification by mobile process"/>
+<node CREATED="1334231049373" ID="ID_494907376" MODIFIED="1334231069748" TEXT="Verification was easy, I needed to go through Captcha human verification again"/>
+<node CREATED="1334231110809" ID="ID_999537231" MODIFIED="1334231124027" TEXT="Heroku configuration - chosen the Python option">
+<node CREATED="1334231135094" ID="ID_1180185658" MODIFIED="1334231137971" TEXT="Your app has been set up! Your provider will send you an email with instructions for how to manage your app environment."/>
+</node>
+<node CREATED="1334231171305" ID="ID_481435366" MODIFIED="1334231182630" TEXT="Go to App">
+<arrowlink DESTINATION="ID_1068195550" ENDARROW="Default" ENDINCLINATION="194;0;" ID="Arrow_ID_1785911908" STARTARROW="None" STARTINCLINATION="194;0;"/>
+</node>
+</node>
+</node>
+<node CREATED="1334231176322" ID="ID_1068195550" MODIFIED="1334231182630" TEXT="https://simple-warrior-9220.herokuapp.com/">
+<node CREATED="1334231283724" ID="ID_479882200" MODIFIED="1334231294481" TEXT="Info about how to edit my App"/>
+<node CREATED="1334231295835" ID="ID_745124306" MODIFIED="1334231299931" TEXT="Log in via FB"/>
+<node CREATED="1334231349277" ID="ID_1452408351" MODIFIED="1334231372872" TEXT="Learn how to edit this App">
+<arrowlink DESTINATION="ID_1297702920" ENDARROW="Default" ENDINCLINATION="173;0;" ID="Arrow_ID_1078668521" STARTARROW="None" STARTINCLINATION="173;0;"/>
+</node>
+</node>
+<node CREATED="1334231356687" ID="ID_1297702920" MODIFIED="1334231372872" TEXT="https://devcenter.heroku.com/articles/facebook">
+<node CREATED="1334231468921" ID="ID_610302335" MODIFIED="1334231478671" TEXT="Going through Heroku Account and Tools Setup">
+<node CREATED="1334232270816" ID="ID_1731567122" MODIFIED="1334232289778" TEXT="Waiting for an email which will allow to create my password on Heroku"/>
+<node CREATED="1334232297883" ID="ID_166219356" MODIFIED="1334232390092" TEXT="No email for 15 minutes"/>
+<node CREATED="1334232391212" ID="ID_766597386" MODIFIED="1334232398958" TEXT="No link to resend the email"/>
+<node CREATED="1334232401205" ID="ID_1439782443" MODIFIED="1334232413703" TEXT="Going to heroku to do it"/>
+<node CREATED="1334232500183" ID="ID_1382556783" MODIFIED="1334233440332" TEXT="Reset Password"/>
+<node CREATED="1334233441355" ID="ID_1776554868" MODIFIED="1334233479999" TEXT="Another 15 minutes passed - sending polite goodbye email to heroku"/>
+<node CREATED="1334234007703" ID="ID_540721920" MODIFIED="1334234023025" TEXT="The response from support@heroku.com received immediately"/>
+</node>
+</node>
+<node CREATED="1334233711292" ID="ID_515157392" MODIFIED="1334233723615" TEXT="Trying to change the hosting settings of the App">
+<node CREATED="1334233988617" ID="ID_952760945" MODIFIED="1334234034756" TEXT="Deleted all hosting addresses"/>
+<node CREATED="1334234035215" ID="ID_290007925" MODIFIED="1334234040353" TEXT="Deleted Website support"/>
+<node CREATED="1334234041587" ID="ID_1441540614" MODIFIED="1334234071937" TEXT="Addedd bugzilla-search.zalewa.info to Canvas URL"/>
+<node CREATED="1334234072543" ID="ID_176254590" MODIFIED="1334234102292" TEXT="Added bugzilla-search.zalewa.info/page-tab to Page Tab URL"/>
+<node CREATED="1334234102875" ID="ID_1152588250" MODIFIED="1334234120442" TEXT="Received an info that https will be required since the 1st October 2012"/>
+<node CREATED="1334234135305" ID="ID_850146197" MODIFIED="1334234150280" TEXT="Creating required hosting on my server"/>
+<node CREATED="1334235146342" ID="ID_1449020947" MODIFIED="1334235164991" TEXT="http://bugzilla-search.zalewa.info is now responding with &quot;Hello World&quot; string"/>
+<node CREATED="1334235682779" ID="ID_1671967244" MODIFIED="1334235714210" TEXT="The URL has to point to a directory or dynamic page (must end with &quot;/&quot; or has &quot;?&quot; somewhere"/>
+</node>
+<node CREATED="1334235891240" ID="ID_1652917723" MODIFIED="1334235946926" TEXT="http://apps.facebook.com/bugzilla-search">
+<node CREATED="1334235909374" ID="ID_1029109081" MODIFIED="1334235968178" TEXT="It is displaying &quot;405 Not Allowed&quot; although the iframe displayed on its own is working fine showing &quot;Hello Canvas&quot;">
+<node CREATED="1334236157138" ID="ID_1096559894" MODIFIED="1334236168415" TEXT="It is fine as FB is sending a POST request"/>
+</node>
+</node>
+<node CREATED="1334235329772" ID="ID_1198736062" MODIFIED="1334236520795" TEXT="Back to the tutorial">
+<node CREATED="1334236522008" ID="ID_725004267" MODIFIED="1334236535796" TEXT="Reading about oAuth dialogue authentication"/>
+<node CREATED="1334236540614" ID="ID_1399023629" MODIFIED="1334236544314" TEXT="having a break"/>
+</node>
+</node>
+</node>
+</map>
